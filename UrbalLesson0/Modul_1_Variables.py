e3657b053c916abfc6df80d6a9a8c57561c47fa1
Alex <prsnl.alex@gmail.com>
2024-10-24 18:21:56 +1000

    24/10/24

---
+++ UrbalLesson0/Modul_1_Variables.py
@@ -0,0 +1,10 @@
+homeworks_completed = 12
+hours = 1.5
+cource_name = 'Python'
+one_task_time = hours / homeworks_completed
+
+print('Курс: ', cource_name, ', ',  'всего задач: ', homeworks_completed, ', '+
+      'затрачено часов: ', hours, ','+ ' среднее время выполнения: ', one_task_time, ' часа'+
+      ', то есть - ', one_task_time*60, 'мин.', sep='')
+
+# END
