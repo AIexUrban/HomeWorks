6457bc4dd8b053c3a023ea04a62665628995b351
Alex <prsnl.alex@gmail.com>
2024-10-26 15:58:28 +1000

    26/10/24

--- UrbalLesson0/Modul_1_Strings&Indexes.py
+++ UrbalLesson0/Modul_1_Strings&Indexes.py
@@ -1,13 +1,18 @@
 # Strings & Indexes
 
 example = 'Топинамбур'
+print(len(example))
 
 print(example[0]) # первый символ этой строки
 print(example[-1]) # последний символ этой строки (используя отрицательный индекс)
 
 i=len(example) # вторую половину этой строки
 a=i//2
-print(example[a:])
+if len(example[a:])%2==0: # проверка на нечетность кол-ва букв
+    print(example[a+1:])  # при выводе второй половины строки
+else:
+    print(example[a:])
+
 
 print(example[::-1]) # это слово наоборот
 print(example[1::2]) # каждый второй символ этой строки
