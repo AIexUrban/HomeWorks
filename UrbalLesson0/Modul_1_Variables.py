7d4bb9118359471ea83f04ea40bab12505cdbabd
Alex <prsnl.alex@gmail.com>
2024-10-26 16:01:42 +1000

    26/10/24

--- UrbalLesson0/Modul_1_Variables.py
+++ UrbalLesson0/Modul_1_Variables.py
@@ -5,6 +5,8 @@ one_task_time = hours / homeworks_completed
 
 print('Курс: ', cource_name, ', ',  'всего задач: ', homeworks_completed, ', '+
       'затрачено часов: ', hours, ','+ ' среднее время выполнения: ', one_task_time, ' часа'+
-      ', то есть - ', one_task_time*60, 'мин.', sep='')
+      ', то есть - ', one_task_time*60, ' мин.', sep='')
+print(f'Курс: {cource_name}, всего задач: {homeworks_completed}, затрачено часов: {hours},'
+      f' среднее время выполнения: {one_task_time} часа, то есть - {one_task_time*60} мин.')
 
 # END
