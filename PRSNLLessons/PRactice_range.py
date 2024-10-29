046408ddcb73c5d47c5c7dd37ada675f373419ac
Alex <prsnl.alex@gmail.com>
2024-10-30 09:44:50 +1000

    29/10/24

---
+++ PRSNLLessons/PRactice_range.py
@@ -0,0 +1,13 @@
+number=range(10)
+print(number)
+
+num=list(number)
+print(num)
+
+# прибавляем единицу к каждому элементу строки
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+for i in range(len(numbers)):
+    numbers[i] += 1
+
+print(numbers)
