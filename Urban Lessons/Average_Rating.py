5dd70cb1ffa10d7d3abdda17dc9c50f57880a85d
Alex <prsnl.alex@gmail.com>
2024-10-30 16:05:01 +1000

    30/10/24

--- Urban Lessons/Average_Rating.py
+++ Urban Lessons/Average_Rating.py
@@ -3,12 +3,10 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-grades_average = [(sum(i) / len(i)) for i in grades] # создаем новый список со средними
-# значениями каждого элемента из списка grades
+students_list=sorted(students) # сортируем множество по алфавиту
 
-students_list=list(students) # преобразуем множество в список для дальнейшей сортировки
-students_list.sort() # Сортируем список по алфавиту
-result = dict(zip(students_list , grades_average)) # объединяем два списка в один словарь
+result = dict(zip(students_list , ([(sum(i) / len(i)) for i in grades]))) # объединяем
+# два списка в один словарь с усредненным значением к каждому ключу
 
 print(result)
 
