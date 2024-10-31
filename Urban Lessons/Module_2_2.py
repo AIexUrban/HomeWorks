73f5065b90101970dc40ff4808a527283e22a9c3
AIexUrban <prsnl.alex@gmail.com>
2024-10-31 22:55:03 +1000

    31/10/24

---
+++ Urban Lessons/Module_2_2.py
@@ -0,0 +1,18 @@
+print('Введите три числа: ')
+
+first, second, third = input(), input(), input()
+if first == second == third:
+    print('3')
+elif first == second or first == third or second == third:
+    print('2')
+else:
+    print('0')
+
+#END
+
+
+
+
+
+
+
